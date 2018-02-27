b64bf40b1b473339c61100df4570a6b374e39817
Christopher Woodward <christopherwoodward@Christophers-iMac.local>
2018-02-26 16:22:48 -0800

    added auth and training components with subs

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,11 @@
 import {NgModule} from '@angular/core';
+import { MatButtonModule } from  '@angular/material';
+import { MatIconModule} from '@angular/material';
 
-@NgModule(
-
-)
+@NgModule({
+  imports: [MatButtonModule, MatIconModule],
+  exports: [MatButtonModule, MatIconModule]
+})
 export class MaterialModule {
 
 }
