ad0cf95c72229e3254609c116728f0698e013671
Christopher Woodward <christopherwoodward@Christophers-iMac.local>
2018-02-27 23:31:45 -0800

    Passed data into dialog and get data out

--- src/app/training/current-training/stop-training/stop-training.component.ts
+++ src/app/training/current-training/stop-training/stop-training.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA} from '@angular/material';
 
 @Component({
   selector: 'app-stop-training',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StopTrainingComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(MAT_DIALOG_DATA) public passedData: any) { }
 
   ngOnInit() {
   }
